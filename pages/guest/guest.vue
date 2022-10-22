01d8eee32e2f713466d070cf7a359eac4f5caa83
隼 <email@sample.com>
2022-10-22 16:48:43 +0800

    导航栏

---
+++ pages/guest/guest.vue
@@ -0,0 +1,19 @@
+<template>
+  <view>
+    Guest
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+
+      };
+    }
+  }
+</script>
+
+<style lang="scss">
+
+</style>
