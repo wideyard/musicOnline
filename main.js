541862a7890f25cc396223a36deae1702327cd01
隼 <email@sample.com>
2022-10-25 00:54:56 +0800

    --story=1007389 --user=陈思妤 头条功能 https://www.tapd.cn/60697868/s/1020767

--- main.js
+++ main.js
@@ -1,20 +1,39 @@
-
 // #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
+// 网络请求包
+import {
+  $http
+} from '@escook/request-miniprogram'
 
+//uni.$http = $http
+//
+//$http.baseUrl = '这里面填资源url'
+//// 请求拦截器
+//$http.beforeRequest = function(options) {
+//  uni.showLoading({
+//    title: 'loading ...'
+//  })
+//}
+//
+//// 响应拦截器
+//$http.afterRequest = function() {
+//  uni.hideLoading()
+//}
 Vue.config.productionTip = false
 
 App.mpType = 'app'
 
 const app = new Vue({
-    ...App
+  ...App
 })
 app.$mount()
 // #endif
 
 // #ifdef VUE3
-import { createSSRApp } from 'vue'
+import {
+  createSSRApp
+} from 'vue'
 import App from './App.vue'
 export function createApp() {
   const app = createSSRApp(App)
--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,19 +1,74 @@
 <template>
-  <view>
-    Home
+  <view class="home">
+    <swiper :indicator-dots="true" :autoplay="true" :interval="2500" :duration="1000" :circular="true">
+      <!-- 循环遍历数组 -->
+      <swiper-item v-for="item in swipers">
+        <image :src="item"></image>
+      </swiper-item>
+    </swiper>
   </view>
 </template>
 
+<!-- 动态绑定 -->
+<!--<template>
+  <view>
+    <!-- 轮播图区域 -->
+<!--    <swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" :circular="true">
+     <!-- 循环渲染轮播图的 item 项 -->
+<!--      <swiper-item v-for="(item, i) in swiperList" :key="i">
+        <view class="swiper-item">
+          <!-- 动态绑定图片的 src 属性 -->
+<!--          <image :src="item.image_src"></image>
+        </view>
+      </swiper-item>
+    </swiper>
+  </view>
+</template>
+-->
 <script>
   export default {
     data() {
       return {
-        
+        // 头条轮播
+        swipers:[
+        					'https://hakaimg.com/i/2022/10/25/puolq.jpg',
+        					'https://hakaimg.com/i/2022/10/25/putua.jpg',
+        					'https://hakaimg.com/i/2022/10/25/pv0lf.jpg',
+        				]
+
+
       };
-    }
+    },
+
+    //onLoad() {
+    //  this.getSwiperlist()
+    //},
+    //methods: {
+    //  async getSwiperlist() {
+    //    const res = await uni.$http.get('里面写你的资源路径 比如/api/public/v1/home/swiperdata')
+    //    // 请求失败
+    //    if (res.meta.status !== 200) {
+    //      return uni.showToast({
+    //        title: '数据请求失败！',
+    //        duration: 1500,
+    //        icon: 'none',
+    //      })
+    //    }
+    //    // 请求成功，为 data 中的数据赋值
+    //    this.swiperList = res.message
+    //  }
+    //}
   }
 </script>
 
 <style lang="scss">
+  swiper {
+    height: 400rpx;
 
+    .swiper-item,
+    image {
+      width: 100%;
+      height: 100%;
+    }
+  }
 </style>
